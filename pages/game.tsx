6469b9ce7baaaca2214519cf9978b93a351b18d5
Markeljan <Markeljan19@gmail.com>
2022-10-24 02:02:18 -0400

    begin quest

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,13 +1,4 @@
-import {
-  Button,
-  Flex,
-  Image,
-  Input,
-  Link,
-  SimpleGrid,
-  Text,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Button, Flex, Image, Link, SimpleGrid, Text, useColorModeValue } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import ToggleTheme from "../components/ToggleTheme";
 import { getUser } from "../auth.config";
@@ -19,7 +10,20 @@ import { ChainID, Unit } from "@harmony-js/utils";
 import { useEffect, useState } from "react";
 import { useLogout } from "@thirdweb-dev/react";
 import { UseContractConfig } from "wagmi/dist/declarations/src/hooks/contracts/useContract";
-import { BsToggleOff, BsToggleOn } from "react-icons/bs";
+import {
+  BsArrowLeft,
+  BsArrowRight,
+  BsFillVolumeMuteFill,
+  BsFillVolumeUpFill,
+  BsToggleOff,
+  BsToggleOn,
+  BsVolumeMute,
+  BsVolumeMuteFill,
+  BsVolumeOff,
+  BsVolumeUp,
+} from "react-icons/bs";
+import Sound from "react-sound";
+import { useAddRecentTransaction } from "@rainbow-me/rainbowkit";
 
 const PRIVATE_KEY = PRIVATE_KEY_HACK;
 
@@ -51,7 +55,7 @@ interface NFT {
 }
 
 export default function GatedPage() {
-  const formBackground = useColorModeValue("gray.100", "gray.700");
+  const formBackground = useColorModeValue("gray.150", "gray.700");
   const buttonBackground = useColorModeValue("blue.200", "blue.600");
   const buttonHoverBackground = useColorModeValue("blue.300", "blue.700");
   const buttonActiveBackground = useColorModeValue("blue.400", "blue.800");
@@ -60,23 +64,37 @@ export default function GatedPage() {
   const { address } = useAccount();
   const [totalSupply, setTotalSupply] = useState(0);
   const [NFTsArray, setNFTsArray] = useState([] as NFT[]);
+  const [userNFTsArray, setUserNFTsArray] = useState([] as NFT[]);
+  const [selectedNFT, setSelectedNFT] = useState<NFT>();
+  const [selectedNFTEl, setSelectedNFTEl] = useState();
   const [NFTEls, setNFTEls] = useState();
   const [userNFTEls, setUserNFTEls] = useState();
   const [showTavern, setShowTavern] = useState(false);
   const [showQuests, setShowQuests] = useState(false);
-  const [selectedTokenId, setSelectedTokenId] = useState(0);
   const [filterNFTs, setFilterNFTs] = useState(false);
+  const [muted, setMuted] = useState(false);
+
+  const addRecentTransaction = useAddRecentTransaction();
+
   const { config } = usePrepareContractWrite({
     address: DAOSCAPE_CONTRACT,
     chainId: 0x6357d2e0,
     abi: DAOSCAPE_ABI,
     functionName: "beginQuest",
-    args: [selectedTokenId],
+    args: [selectedNFT?.id],
     overrides: {
       gasPrice: 600000000000,
     },
   } as UseContractConfig);
   const { data, isLoading, isSuccess, write } = useContractWrite(config);
+
+  useEffect(() => {
+    data &&
+      addRecentTransaction({
+        hash: data.hash,
+        description: "Quest Started",
+      });
+  }, [data]);
 
   useEffect(() => {
     isDisconnected && logout();
@@ -109,10 +127,23 @@ export default function GatedPage() {
 
   useEffect(() => {
     if (NFTsArray) {
+      let tempUserNFTArray = [] as NFT[];
+      NFTsArray.forEach((nft) => {
+        if (nft.owner === address?.toLocaleLowerCase()) {
+          tempUserNFTArray.push(nft);
+        }
+      });
+      setUserNFTsArray(tempUserNFTArray);
+
       setNFTEls(renderAllNFTs() as any);
       setUserNFTEls(renderUserNFTs() as any);
+      setSelectedNFT(NFTsArray.find((nft) => nft.owner === address!.toLocaleLowerCase()) as NFT);
     }
   }, [NFTsArray]);
+
+  useEffect(() => {
+    selectedNFT && setSelectedNFTEl(renderSelectedNFTEl() as any);
+  }, [selectedNFT]);
 
   function renderAllNFTs() {
     return NFTsArray.map((nft) => {
@@ -139,8 +170,6 @@ export default function GatedPage() {
 
   function renderUserNFTs() {
     return NFTsArray.map((nft) => {
-      console.log(nft);
-      console.log(address);
       if (nft.owner === address!.toLocaleLowerCase())
         return (
           <Flex key={nft.id} direction="column" align="center" justify="center">
@@ -163,7 +192,51 @@ export default function GatedPage() {
     });
   }
 
-  function startQuest() {}
+  function renderSelectedNFTEl() {
+    return (
+      <Flex width={"200px"} direction="column">
+        <Image src={selectedNFT!.uri} />
+        <Text>{"Token id: " + selectedNFT!.id}</Text>
+        <Text>
+          {"Owner: "}
+          <Link href={"https://explorer.pops.one/address/" + selectedNFT!.owner} isExternal>
+            {selectedNFT!.owner.substring(0, 5) + "..." + selectedNFT!.owner.substring(39, 42)}
+          </Link>
+        </Text>
+        <Text>
+          {"URI: "}
+          <Link href={selectedNFT!.uri} isExternal>
+            {selectedNFT!.uri.substring(0, 20)}
+          </Link>
+        </Text>
+        <Flex justify={"space-between"}>
+          <Button
+            onClick={() =>
+              setSelectedNFT(
+                userNFTsArray.findIndex((nft) => nft.id === selectedNFT!.id) === 0
+                  ? userNFTsArray[userNFTsArray.length - 1]
+                  : userNFTsArray[userNFTsArray.findIndex((nft) => nft.id === selectedNFT!.id) - 1]
+              )
+            }
+          >
+            <BsArrowLeft size={"40px"} />
+          </Button>
+          <Button
+            onClick={() =>
+              setSelectedNFT(
+                userNFTsArray.findIndex((nft) => nft.id === selectedNFT!.id) ===
+                  userNFTsArray.length - 1
+                  ? userNFTsArray[0]
+                  : userNFTsArray[userNFTsArray.findIndex((nft) => nft.id === selectedNFT!.id) + 1]
+              )
+            }
+          >
+            <BsArrowRight size={"40px"} />
+          </Button>
+        </Flex>
+      </Flex>
+    );
+  }
 
   return (
     <>
@@ -186,16 +259,25 @@ export default function GatedPage() {
             position={"relative"}
           >
             <Image width="100%" src="/gameMap.jpg" />
+            <Flex
+              p={3}
+              m={1}
+              borderRadius="2xl"
+              background={useColorModeValue(buttonBackground, buttonBackground)}
+              position={"absolute"}
+            >
+              {selectedNFTEl}
+            </Flex>
             <Link href="https://docs.daoscape.one/home/gameplay/combat" isExternal>
               <Button
                 position={"absolute"}
+                fontSize={"20px"}
                 top="28%"
                 left="30%"
                 background={buttonBackground}
                 opacity="0.9"
                 _hover={{ background: buttonHoverBackground }}
                 _active={{ background: buttonActiveBackground }}
-                textColor="white"
                 p="2, 4"
                 rounded="md"
               >
@@ -204,6 +286,7 @@ export default function GatedPage() {
             </Link>
             <Button
               onClick={() => setShowQuests(!showQuests)}
+              fontSize={"20px"}
               position={"absolute"}
               top="25%"
               left="75%"
@@ -211,7 +294,6 @@ export default function GatedPage() {
               opacity="0.9"
               _hover={{ background: buttonHoverBackground }}
               _active={{ background: buttonActiveBackground }}
-              textColor="white"
               p="2, 4"
               rounded="md"
             >
@@ -220,6 +302,7 @@ export default function GatedPage() {
 
             <Link href="https://docs.daoscape.one/home/gameplay/training" isExternal>
               <Button
+                fontSize={"20px"}
                 position={"absolute"}
                 top="80%"
                 left="40%"
@@ -227,7 +310,6 @@ export default function GatedPage() {
                 opacity="0.9"
                 _hover={{ background: buttonHoverBackground }}
                 _active={{ background: buttonActiveBackground }}
-                textColor="white"
                 p="2, 4"
                 rounded="md"
               >
@@ -237,6 +319,7 @@ export default function GatedPage() {
 
             <Button
               onClick={() => setShowTavern(!showTavern)}
+              fontSize={"20px"}
               position={"absolute"}
               top="55%"
               left="62%"
@@ -244,7 +327,6 @@ export default function GatedPage() {
               opacity="0.9"
               _hover={{ background: buttonHoverBackground }}
               _active={{ background: buttonActiveBackground }}
-              textColor="white"
               p="2, 4"
               rounded="md"
             >
@@ -253,6 +335,7 @@ export default function GatedPage() {
 
             <Link href="https://docs.daoscape.one/home/gameplay/combat" isExternal>
               <Button
+                fontSize={"20px"}
                 position={"absolute"}
                 top="45%"
                 left="82%"
@@ -260,7 +343,6 @@ export default function GatedPage() {
                 opacity="0.9"
                 _hover={{ background: buttonHoverBackground }}
                 _active={{ background: buttonActiveBackground }}
-                textColor="white"
                 p="2, 4"
                 rounded="md"
               >
@@ -271,6 +353,7 @@ export default function GatedPage() {
 
           <Flex
             hidden={showTavern ? false : true}
+            fontSize={"20px"}
             direction={"column"}
             width="100%"
             border="4px dotted gray"
@@ -294,8 +377,8 @@ export default function GatedPage() {
             </Button>
 
             <SimpleGrid
-              columns={{ base: 1, md: 2, lg: 3 }}
-              spacing={10}
+              columns={{ base: 2, md: 3, lg: 4 }}
+              spacing={"40px"}
               background={formBackground}
             >
               {filterNFTs ? userNFTEls : NFTEls}
@@ -315,31 +398,44 @@ export default function GatedPage() {
             pr={10}
             gap={4}
           >
-            <Button onClick={() => setShowQuests(!showQuests)} fontSize={"3xl"}>
-              Exit Quests
-            </Button>
-            <Text fontSize={"xl"}>
-              Send your Scapers on quests to earn experience, loot and/or DAOGold!
-            </Text>
+            <Flex>
+              <Button onClick={() => setShowQuests(!showQuests)} fontSize={"3xl"}>
+                Exit Quests
+              </Button>
+            </Flex>
+            <Flex
+              p={3}
+              borderRadius="2xl"
+              background={useColorModeValue(buttonActiveBackground, buttonActiveBackground)}
+            >
+              {selectedNFTEl}
+            </Flex>
+
+            <Text fontSize={"xl"}>Send your Scapers on quests to earn EXP, and DAOGold!</Text>
             <Text fontSize={"lg"}>(60 min lock period.)</Text>
-            <Input
-              onChange={(e) => setSelectedTokenId(Number(e.target.value))}
-              value={selectedTokenId}
-              width={"200px"}
-              placeholder="Scaper tokenId"
-            ></Input>
             <SimpleGrid
-              columns={{ base: 1, md: 2, lg: 3 }}
+              columns={{ base: 1, md: 2, lg: 2 }}
               spacing={10}
               background={formBackground}
             >
-              <Button onClick={() => startQuest()}>Start Quest</Button>
+              <Button onClick={() => write?.()}>Start Quest</Button>
               <Button>End Quest</Button>
             </SimpleGrid>
           </Flex>
         </Flex>
       </Flex>
-      <ToggleTheme />
+      <Flex justify={"space-between"} align="center">
+        <ToggleTheme />
+        <Button
+          m={6}
+          onClick={() => {
+            setMuted(!muted);
+          }}
+        >
+          {muted ? <BsVolumeMuteFill size={30} /> : <BsFillVolumeUpFill size={30} />}
+        </Button>
+      </Flex>
+      <Sound url="/osrs.mp3" playStatus="PLAYING" volume={muted ? 0 : 40} />
     </>
   );
 }
